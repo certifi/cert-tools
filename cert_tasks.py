f57126d56cd82204b196cbe1124fe1a7470d66f0
Kenneth Reitz <me@kennethreitz.com>
2014-04-17 10:07:59 -0400

    cleanup cert_tasks

--- cert_tasks.py
+++ cert_tasks.py
@@ -1,18 +1,22 @@
+
+import os
+import hashlib
+
+import requests
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from invoke import task, run
-import hashlib
-import requests
+
+MOZILLA_BUNDLE = 'https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt'
 
 @task
-def generate(filename=''):
-    filename = filename if filename else 'cacerts.pem'
-    run('curl https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt -o certdata.txt')
+def generate(filename='cacerts.pem'):
+
+    run('curl {} -o certdata.txt'.format(MOZILLA_BUNDLE))
     run("extract-nss-root-certs > '%s'" % filename)
 
 @task
-def diff(gen=False, filename=''):
-    filename = filename if filename else 'cacerts.pem'
+def diff(gen=False, filename='cacerts.pem'):
 
     if gen:
         generate(filename)
@@ -28,22 +32,30 @@ def diff(gen=False, filename=''):
         print 'No change.'
     else:
         print 'Certs are different, you should upload.'
+        exit(1)
+        # TODO: --upload  e.g. && upload()
 
 @task
-def upload(filename=''):
-    filename = filename if filename else 'cacerts.pem'
+def upload(filename='cacerts.pem'):
 
-    # This method requires the environment variables AWS_ACCESS_KEY_ID and
-    # AWS_SECRET_ACCESS_KEY to be set appropriately.
-    conn = S3Connection()
-    b = conn.get_bucket('certifi-bundles')
-    k = Key(b)
+    # This method requires the environment variables to be set appropriately:
+    # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_BUCKET_NAME.
+
+    BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
+
+    s3 = S3Connection()
+    bucket = s3.get_bucket(BUCKET_NAME)
+
+
+    k = Key(bucket)
 
     with open('cacerts.pem', 'rb') as f:
         k.key = hashlib.sha1(f.read()).hexdigest() + '.pem'
 
-    k.set_contents_from_filename('cacerts.pem')
+    k.set_contents_from_filename(filename)
 
-    k = Key(b)
+    # TODO: setup object redirect.
+
+    k = Key(bucker)
     k.key = 'latest.pem'
-    k.set_contents_from_filename('cacerts.pem')
+    k.set_contents_from_filename(filename)
